5c52fcffdf7bdde777712075da2523f71513e6a3
Gener Almestica <83323690+otenkoo@users.noreply.github.com>
2023-03-21 09:46:37 -0400

    Added Sign In page and Preliminary Links

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ServicesPage from "./pages/ServicesPage";
 import ContactPage from "./pages/ContactPage";
-
+import SignInPage from "./pages/SignInPage";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 export default function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/services" element={<ServicesPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-    </Routes>
-  )
+    <div>
+      <ResponsiveAppBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+      </Routes>
+    </div>
+  );
 }
-
